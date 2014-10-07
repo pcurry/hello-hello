72ef48cb0e6611257df330d316d9bee7d30d5034
Paul Curry <pcurry@gmail.com>
2014-10-06 23:07:15 -0400

    The ones I could do off the top of my head. I'm ashamed there aren't more.

---
+++ NoviceGreeter.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+
+public class NoviceGreeter {
+
+    public NoviceGreeter() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
